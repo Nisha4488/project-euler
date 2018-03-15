238c7720a18d6ecfc326bf0e7ce2557f31867367
Nisha4488 <connect.your.app@gmail.com>
2018-03-14 23:54:12 -0700

    Code to add multiple of 3 or 5

--- SumOfMultiple.js
+++ SumOfMultiple.js
@@ -1,101 +1,46 @@
-/**
- * Simple approach to find out the sum of 3 or 5 multiple
- * @param num
- * @returns {number}
- */
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.Find the sum of all the multiples of 3 or 5 below 1000.
+
+// Simple approach to find out the sum of 3 or 5 multiple
+
 function sumOfMultiple1(num) {
-  var startTime = new Date().getTime();
   var sum = 0;
   var addition = [];
-  var i;
 
-  // Find out multiple or 3 and add into list
-  for (i = 0; i < num; i++) {
+// Finds multiple Of 3 and add into list
+  for (var i = 0; i < num; i++) {
     if (i % 3 === 0) {
       addition.push(i);
     }
   }
 
-  // Find out multiple or 3 and add into list
+// Finds multiple of 5 and add into list
   for (i = 0; i < num; i++) {
     if (i % 5 === 0 && i % 3 !== 0) {
       addition.push(i);
     }
   }
-  // Simply sum  the list
+// Simply sum  the list
   for (i = 0; i < addition.length; i++) {
     sum = sum + addition[i];
   }
-  console.log('Time taken ', new Date().getTime() - startTime);
   return sum;
 }
+console.log('\n');
+console.log("Answer of sumOfMultiple1:",sumOfMultiple1(100));
+console.log("Answer of sumOfMultiple1:",sumOfMultiple1(20));
+
+//Answer of sumOfMultiple1: 2318
+//Answer of sumOfMultiple1: 78
 
 
-console.log('sumOfMultiple1: Sum of Multiple for 10 is ', sumOfMultiple1(10));
-console.log('sumOfMultiple1: Sum of Multiple for 20 is ', sumOfMultiple1(20));
-console.log('sumOfMultiple1: Sum of Multiple for 1000 is ', sumOfMultiple1(1000));
-console.log('sumOfMultiple1: Sum of Multiple for 100000000 is ', sumOfMultiple1(100000000))
 
-
-/** Instead of iterating from 0 to num, let's iterate as a multiple of 3 or 5
- * @param num
- * @returns {number}
- */
+//iterative approach from 0 to num, however used one loop block for both 3 and 5
 
 function sumOfMultiple2(num) {
-  var startTime = new Date().getTime();
   var sum = 0;
   var addition = [];
-  var i;
-  // iterate multiple of 3 and add into list
-  for (i = 1; true; i++) {
-    var multiple = 3 * i;
-    if (multiple < num) {
-      addition.push(multiple);
-    } else {
-      break;
-    }
-  }
 
-  for (i = 1; true; i++) {
-    multiple = 5 * i;
-    if (multiple % 3 !== 0) {
-      if (multiple < num) {
-        addition.push(multiple);
-      } else {
-        break;
-      }
-    }
-
-  }
-  // Simply sum  the list
-  for (i = 0; i < addition.length; i++) {
-    sum = sum + addition[i];
-  }
-  console.log('Time taken ', new Date().getTime() - startTime);
-  return sum;
-}
-
-console.log('\n\n');
-
-
-console.log('sumOfMultiple2: Sum of Multiple for 10 is ', sumOfMultiple2(10));
-console.log('sumOfMultiple2: Sum of Multiple for 20 is ', sumOfMultiple2(20));
-console.log('sumOfMultiple2: Sum of Multiple for 1000 is ', sumOfMultiple2(1000));
-console.log('sumOfMultiple2: Sum of Multiple for 100000000 is ', sumOfMultiple2(100000000));
-
-/**
- * Use iterative approach from 0 to num, but use one loop block for both 3 and 5
- * @param num
- * @returns {number}
- */
-
-function sumOfMultiple3(num) {
-  var startTime = new Date().getTime();
-  var sum = 0;
-  var addition = [];
-  var i;
-  for (i = 0; i < num; i++) {
+  for (var i = 0; i < num; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
       addition.push(i);
     }
@@ -105,97 +50,12 @@ function sumOfMultiple3(num) {
   for (i = 0; i < addition.length; i++) {
     sum = sum + addition[i];
   }
-  console.log('Time taken ', new Date().getTime() - startTime);
   return sum;
 }
+console.log('\n');
 
-console.log('\n\n');
+console.log("Answer of sumOfMultiple2:",sumOfMultiple2(100));
+console.log("Answer of sumOfMultiple2:",sumOfMultiple2(20));
 
-console.log('sumOfMultiple3: Sum of Multiple for 10 is ', sumOfMultiple3(10));
-console.log('sumOfMultiple3: Sum of Multiple for 20 is ', sumOfMultiple3(20));
-console.log('sumOfMultiple3: Sum of Multiple for 1000 is ', sumOfMultiple3(1000));
-console.log('sumOfMultiple3: Sum of Multiple for 100000000 is ', sumOfMultiple3(100000000));
-
-/**
- * iterate as a multiple of 3 or 5 but use one block for both 3 and 5
- * @param num
- * @param @returns {number}
- */
-
-function sumOfMultiple4(num) {
-  var startTime = new Date().getTime();
-  var sum = 0;
-  var addition = [];
-  var i;
-  for (i = 1; true; i++) {
-    var multipleOf3 = 3 * i;
-    var multipleOf5 = 5 * i;
-
-    if (multipleOf3 < num) {
-      addition.push(multipleOf3);
-    }
-
-    if (multipleOf5 < num && multipleOf5 % 3 !== 0) {
-      addition.push(multipleOf5);
-    }
-
-    if (multipleOf3 > num && multipleOf5 > num) {
-      break;
-    }
-
-  }
-  // Simply sum  the list
-  for (i = 0; i < addition.length; i++) {
-    sum = sum + addition[i];
-  }
-  console.log('Time taken ', new Date().getTime() - startTime);
-  return sum;
-}
-
-console.log('\n\n');
-console.log('sumOfMultiple4: Sum of Multiple for 10 is ', sumOfMultiple4(10));
-console.log('sumOfMultiple4: Sum of Multiple for 20 is ', sumOfMultiple4(20));
-console.log('sumOfMultiple4: Sum of Multiple for 1000 is ', sumOfMultiple4(1000));
-console.log('sumOfMultiple4: Sum of Multiple for 100000000 is ', sumOfMultiple4(100000000));
-
-
-/** Instead of iterating from 0 to num, let's iterate as a multiple of 3 or 5
- * @param num
- * @returns {number}
- */
-
-function sumOfMultiple5(num) {
-  var startTime = new Date().getTime();
-  var sum = 0;
-  var i;
-  // iterate multiple of 3 and add into list
-  for (i = 1; true; i++) {
-    var multiple = 3 * i;
-    if (multiple < num) {
-      sum = sum + multiple;
-    } else {
-      break;
-    }
-  }
-
-  for (i = 1; true; i++) {
-    multiple = 5 * i;
-    if (multiple % 3 !== 0) {
-      if (multiple < num) {
-        sum = sum + multiple;
-      } else {
-        break;
-      }
-    }
-
-  }
-
-  console.log('Time taken ', new Date().getTime() - startTime);
-  return sum;
-}
-
-console.log('\n\n');
-console.log('sumOfMultiple5: Sum of Multiple for 10 is ', sumOfMultiple5(10));
-console.log('sumOfMultiple5: Sum of Multiple for 20 is ', sumOfMultiple5(20));
-console.log('sumOfMultiple5: Sum of Multiple for 1000 is ', sumOfMultiple5(1000));
-console.log('sumOfMultiple5: Sum of Multiple for 100000000 is ', sumOfMultiple5(100000000));
+//Answer of sumOfMultiple2: 2318
+//Answer of sumOfMultiple2: 78
